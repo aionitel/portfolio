171c27cd46df62c687fc780540b3dda1a8c789fe
Alex Ionitel <oranggsoda@gmail.com>
2024-01-06 19:21:55 -0500

    started wavy video on homepage; also added typewriter effect for home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,36 @@
-<h1 class="font-bold">test</h1>
+<script lang="ts">
+    import Typewriter from "svelte-typewriter";
+</script>
+
+<svelte:head>
+    <title>Alex Ionitel</title>
+</svelte:head>
+<div class="flex-row overflow-hidden relative z-0 text-white h-screen">
+    <video
+        class="flex h-full w-full object-cover"
+        autoPlay
+        muted
+        loop
+        playsInline
+    >
+        <source src="https://i.imgur.com/AAq8AGQ.mp4" />
+    </video>
+    <Typewriter>
+        <h1
+            class="font-alex text-center z-10 inset-0 absolute text-4xl flex justify-center m-0 translate-y-1/4 top-64"
+        >
+            Alex Ionitel's Home Page on the Internet
+        </h1>
+    </Typewriter>
+    <div
+        class="z-10 inset-0 absolute flex justify-center mx-auto text-7xl hover:cursor-pointer top-3/4 h-12"
+        on:click={() =>
+            window.scrollTo({ top: screen.height - 50, behavior: "smooth" })}
+    >
+        <img
+            src="https://cdn-icons-png.flaticon.com/512/271/271228.png"
+            class="rotate-90 invert"
+            alt="down-arrow"
+        />
+    </div>
+</div>
